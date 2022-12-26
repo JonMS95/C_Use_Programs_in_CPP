32d656ec379a7bb559983a6b8357c25f9bd02687
JonMS95 <jon_portugalete@hotmail.com>
2022-12-26 16:41:04 +0100

    Added main.h file.

--- Source_files/C/main.c
+++ Source_files/C/main.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
+#include "main.h"
 #include "../C++/multByTwo.hpp"
 #include "../C++/RectangleInterface.hpp"
-
-#define MSG_WIDTH                   "Rectangle width: %.2f\r\n"
-#define MSG_LENGTH                  "Rectangle length: %.2f\r\n"
-#define MSG_PERIMETER               "Rectangle perimeter: %.2f\r\n"
-#define MSG_AREA                    "Rectangle area: %.2f\r\n"
-#define MSG_NEW_WIDTH_AND_LENGTH    "Width and length of the rectangle are modified now to %f and %f\r\n"
-
-#define NEW_RECTANGLE_WIDTH     10.1
-#define NEW_RECTANGLE_LENGTH    6.3
 
 void printRectangleMeasures(RectangleHandler pRect)
 {
---
+++ Source_files/C/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define MSG_WIDTH                   "Rectangle width: %.2f\r\n"
+#define MSG_LENGTH                  "Rectangle length: %.2f\r\n"
+#define MSG_PERIMETER               "Rectangle perimeter: %.2f\r\n"
+#define MSG_AREA                    "Rectangle area: %.2f\r\n"
+#define MSG_NEW_WIDTH_AND_LENGTH    "Width and length of the rectangle are modified now to %f and %f\r\n"
+
+#define NEW_RECTANGLE_WIDTH     10.1
+#define NEW_RECTANGLE_LENGTH    6.3
+
+#endif
