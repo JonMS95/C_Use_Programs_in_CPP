ddf9d6523c26f615056a296e0f53afc221b5ed32
JonMS95 <jon_portugalete@hotmail.com>
2022-12-26 16:39:44 +0100

    Added some defines in the main.c file, just to make the code look cleaner.

--- Source_files/C/main.c
+++ Source_files/C/main.c
@@ -2,15 +2,21 @@
 #include "../C++/multByTwo.hpp"
 #include "../C++/RectangleInterface.hpp"
 
+#define MSG_WIDTH                   "Rectangle width: %.2f\r\n"
+#define MSG_LENGTH                  "Rectangle length: %.2f\r\n"
+#define MSG_PERIMETER               "Rectangle perimeter: %.2f\r\n"
+#define MSG_AREA                    "Rectangle area: %.2f\r\n"
+#define MSG_NEW_WIDTH_AND_LENGTH    "Width and length of the rectangle are modified now to %f and %f\r\n"
+
 #define NEW_RECTANGLE_WIDTH     10.1
 #define NEW_RECTANGLE_LENGTH    6.3
 
 void printRectangleMeasures(RectangleHandler pRect)
 {
-    printf("Rectangle width: %.2f\r\n", getWidth(pRect));
-    printf("Rectangle length: %.2f\r\n", getLength(pRect));
-    printf("Rectangle perimeter: %.2f\r\n", getPerimeter(pRect));
-    printf("Rectangle area: %.2f\r\n", getArea(pRect));
+    printf(MSG_WIDTH    , getWidth(pRect)       );
+    printf(MSG_LENGTH   , getLength(pRect)      );
+    printf(MSG_PERIMETER, getPerimeter(pRect)   );
+    printf(MSG_AREA     , getArea(pRect)        );
 }
 
 int main()
@@ -22,7 +28,7 @@ int main()
 
     printRectangleMeasures(myRect);
 
-    printf("Width and length of the rectangle are modified now to %f and %f\r\n",
+    printf(MSG_NEW_WIDTH_AND_LENGTH,
     NEW_RECTANGLE_WIDTH,
     NEW_RECTANGLE_LENGTH);
 
